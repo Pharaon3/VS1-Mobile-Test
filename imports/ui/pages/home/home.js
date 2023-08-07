7c6695f2a2ee144241acde98ba5590d04a747ed5
pharaon3 <pharaon313@outlook.com>
2023-08-07 07:40:21 -0400

    popup modal

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -11,21 +11,121 @@ Template.App_home.onCreated(function helloOnCreated() {
   });
   
   Template.App_home.events({
-    'click #transaction'(event, instance) {
+    'click #invoice'(event, instance) {
       // increment the counter when button is clicked
       Swal.fire({
-        title: 'Are you sure?',
-        text: 'You will not be able to recover this imaginary file!',
-        icon: 'warning',
+        title: 'Invoice List',
+        text: 'Now you are in Invoice List',
+        icon: 'info',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
+        confirmButtonText: 'OK, I got it!'
       }).then((result) => {
         if (result.isConfirmed) {
           Swal.fire(
-            'Deleted!',
-            'Your imaginary file has been deleted.',
+            'Thank you!',
+            'Now you are going back to main page.',
+            'success'
+          )
+        }
+      })
+    },
+    'click #Customer'(event, instance) {
+      // increment the counter when button is clicked
+      Swal.fire({
+        title: 'Customer List',
+        text: 'Now you are in Customer List',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'OK, I got it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Thank you!',
+            'Now you are going back to main page.',
+            'success'
+          )
+        }
+      })
+    },
+    'click #Quote'(event, instance) {
+      // increment the counter when button is clicked
+      Swal.fire({
+        title: 'Quote List',
+        text: 'Now you are in Quote List',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'OK, I got it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Thank you!',
+            'Now you are going back to main page.',
+            'success'
+          )
+        }
+      })
+    },
+    'click #Receipt'(event, instance) {
+      // increment the counter when button is clicked
+      Swal.fire({
+        title: 'Receipt Claim',
+        text: 'Now you are in Receipt Claim',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'OK, I got it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Thank you!',
+            'Now you are going back to main page.',
+            'success'
+          )
+        }
+      })
+    },
+    'click #Calendar'(event, instance) {
+      // increment the counter when button is clicked
+      Swal.fire({
+        title: 'Calendar',
+        text: 'Now you are in Calendar',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'OK, I got it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Thank you!',
+            'Now you are going back to main page.',
+            'success'
+          )
+        }
+      })
+    },
+    'click #Clock'(event, instance) {
+      // increment the counter when button is clicked
+      Swal.fire({
+        title: 'Clock On/Off',
+        text: 'Now you are in Clock',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'OK, I got it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Thank you!',
+            'Now you are going back to main page.',
             'success'
           )
         }
